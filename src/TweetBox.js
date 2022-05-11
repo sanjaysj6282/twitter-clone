09d0786c241c9654a7e0bd93566bd1f5e2962245
Sanjay <sanjaysj6282@gmail.com>
2022-05-11 22:34:41 +0530

    tweetbox

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import "./TweetBox.css"
 import {Avatar, Button} from '@mui/material';
+// import db from "./firebase";
 
 function TweetBox() {
   return (
     <div className="tweetbox">
         <form >
             <div className="tweetBox__input">
-               <Avatar src="mypic.jpg"></Avatar>
-               <input type="Whats happening" /> 
+            <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+               <input placeholder="What's happening" type="text"/>Tweet 
             </div>
+            <input placeholder='Enter img url' type="text"/>
+            
+            <Button className='tweetBox__tweetButton'>tweet</Button>
         </form>
     </div>
-  )
+  );
 }
 
-export default TweetBox
+export default TweetBox;
