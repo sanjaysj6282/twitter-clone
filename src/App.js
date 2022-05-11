3878331c059e4a31817a7604c225383e82d8e03c
Sanjay <sanjaysj6282@gmail.com>
2022-05-11 19:42:13 +0530

    Revert "addded sidebar"
    This reverts commit faaa29d22c45a578db8ef4917898120eea70ce2d.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hey twitter</h1>
-      {/* Sidebar */}
-      {/* Feed */}
-      {/* Widget */}
+      
     </div>
   );
 }
