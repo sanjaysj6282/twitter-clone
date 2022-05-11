faaa29d22c45a578db8ef4917898120eea70ce2d
Sanjay <sanjaysj6282@gmail.com>
2022-05-11 19:14:21 +0530

    addded sidebar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>hey twitter</h1>
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widget */}
     </div>
   );
 }
