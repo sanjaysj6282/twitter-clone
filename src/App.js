6dd4068fbde6f38d5300536d26b6852572174675
Sanjay <sanjaysj6282@gmail.com>
2022-05-11 19:45:44 +0530

    added correct

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar></Sidebar>
       <h1>dsgd</h1>
     </div>
   );
