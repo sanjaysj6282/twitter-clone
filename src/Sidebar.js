1e5c3f5d015acdb3590afd6b1fa56f68dce685c6
Sanjay <sanjaysj6282@gmail.com>
2022-05-11 20:37:32 +0530

    addded logo

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,12 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
 
 function Sidebar() {
     return(
@@ -15,6 +21,11 @@ function Sidebar() {
             <SidebarOption Icon={HomeIcon} text="Home"/>
             <SidebarOption Icon={SearchIcon} text="Explore"/>
             <SidebarOption Icon={NotificationsNoneIcon} text="Notifications"/>
+            <SidebarOption Icon={MailOutlineIcon} text="Messages"/>
+            <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks"/>
+            <SidebarOption Icon={ListAltIcon} text="Lists"/>
+            <SidebarOption Icon={PermIdentityIcon} text="Profile"/>
+            <SidebarOption Icon={MoreHorizIcon} text="More"/>
             
         </div>
     );
