0aa5d6ecdc69afe0807856e135ed67d788bb9196
Sanjay <sanjaysj6282@gmail.com>
2022-05-11 21:52:39 +0530

    feed updating

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import "./Feed.css";
 
 function Feed() {
   return (
     <div>
         {/* Header */}
+        <div className="feed"></div>
         <h2>Home</h2>
         {/* TweetBox */}
 
